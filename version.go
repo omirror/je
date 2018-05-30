db4ff70e0b085efb3846d83e16e30a945d8eaf64
James Mills <prologic@shortcircuit.net.au>
2018-05-30 00:41:37 -0700

    Starting to write tests...

--- version.go
+++ version.go
@@ -1,16 +1,21 @@
 package je
 
+import (
+	"fmt"
+)
+
 var (
 	// Version release version
 	Version = "0.0.1"
 
 	// Build will be overwritten automatically by the build system
-	Build = "-dev"
+	Build = "dev"
 
 	// GitCommit will be overwritten automatically by the build system
 	GitCommit = "HEAD"
 )
 
+// FullVersion returns the full version, build and commit hash
 func FullVersion() string {
-	return Version + Build + " (" + GitCommit + ")"
+	return fmt.Sprintf("%s-%s@%s", Version, Build, GitCommit)
 }
