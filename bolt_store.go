29a3704ceb71af29e511a451f860665004870feb
James Mills <prologic@shortcircuit.net.au>
2020-05-07 06:04:50 +1000

    Updated and fixed all dependencies (finally)

--- bolt_store.go
+++ bolt_store.go
@@ -8,7 +8,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/blevesearch/bleve"
-	bolt "github.com/coreos/bbolt"
+	bolt "go.etcd.io/bbolt"
 
 	"github.com/prologic/je/codec"
 	"github.com/prologic/je/codec/json"
